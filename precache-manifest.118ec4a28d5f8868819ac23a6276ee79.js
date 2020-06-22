3f81cce93c9998c6ac9a34794cf2668dc0c3c50f
Robert Sidzinka <robert.sidzinka@abventor.com>
2020-06-22 23:53:28 +0200

    Updates

---
+++ precache-manifest.118ec4a28d5f8868819ac23a6276ee79.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa73bf9611ccb87605d533255aa260a1",
+    "url": "/blog-stalker/index.html"
+  },
+  {
+    "revision": "8cafbd11121aa4323904",
+    "url": "/blog-stalker/static/js/2.d7fc649a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/blog-stalker/static/js/2.d7fc649a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69a34490942d97a64e1f",
+    "url": "/blog-stalker/static/js/main.1104bc4c.chunk.js"
+  },
+  {
+    "revision": "8cabd0aabe13eb96b122",
+    "url": "/blog-stalker/static/js/runtime-main.842b016c.js"
+  }
+]);
